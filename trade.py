be74c6104e1429e480af794945457e63b79e2858
Trev18 <66802083+Trev18@users.noreply.github.com>
2020-06-14 17:16:20 -0500

    Add files via upload

---
+++ trade.py
@@ -0,0 +1,40 @@
+import requests, json
+import config
+#import stream
+
+API_KEY = 'PK1X4W2RZG5TB15V0QGM'
+SECRET_KEY =  'dYUMhwHitUflmI6cWNRu2sQHHwwu1LOh9y5DVBa9'
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID' : API_KEY, 'APCA-API-SECRET-KEY' : SECRET_KEY}
+STOCK_1 = "BA"
+def get_account(): 
+    r = requests.get(ACCOUNT_URL, headers=HEADERS)
+ 
+    return json.loads(r.content)
+
+def create_order(symbol, qty, side, type, time_in_force): #order_class, take_profit.limit_price, stop_price  ): 
+    data = {
+        "symbol": symbol,
+        "qty": qty, 
+        "side": side,
+        "type": type,
+        "time_in_force": time_in_force,
+        #"order_class" : order_class,
+        #"take_profit" : take_profit,
+        #"limit_price": limit_price,
+        #"stop_loss" : stop_loss,
+        #"stop_price": stop_price,
+    }
+
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+
+    return json.loads(r.content)
+
+response = create_order("MSFT", 1, "buy", "market", "gtc")
+#response = create_order("BA", 1, "buy", "market", "gtc")
+
+
+print(response)
