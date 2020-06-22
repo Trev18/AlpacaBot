c8ddfb685a6d34c4ecdd67ca64298758800e79d8
Trev18 <66802083+Trev18@users.noreply.github.com>
2020-06-21 19:34:57 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -5,12 +5,12 @@ def on_open(ws):
     print("opened")
     auth_data = {
         "action": "authenticate",
-        "data": {"key_id": "XXXXXXXXXXXXX", "secret_key": "XXXXXXXXXXXXXXXXXXX"}
+        "data": {"key_id": "PK1X4W2RZG5TB15V0QGM", "secret_key": "dYUMhwHitUflmI6cWNRu2sQHHwwu1LOh9y5DVBa9"}
     }
 
     ws.send(json.dumps(auth_data))
 
-    listen_message = {"action": "listen", "data": {"streams": ["AM.BA"]}}
+    listen_message = {"action": "listen", "data": {"streams": ["T.BA"]}}
 
     ws.send(json.dumps(listen_message))
 
@@ -25,7 +25,7 @@ def on_message(ws, message):
     low_price = message.l
     average_price = message.a
     ticker = message.T
-
+    print(open_price)
 #def on_close(ws):
     #print("closed connection")
 
@@ -33,4 +33,3 @@ socket = "wss://data.alpaca.markets/stream"
 
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message) #on_close=on_close)
 ws.run_forever()
-
