da41452dd245187c32f926fa16462f117e1db0ba
Trev18 <66802083+Trev18@users.noreply.github.com>
2020-06-25 09:54:56 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,35 +1,84 @@
-#import config
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api import StreamConn
+import threading
+import time
+import datetime
+import logging
+import argparse
+import websocket
+import json
+import requests
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+# API KEYS
+
+API_KEY = "XXXXXXXXXXXXXXXXXX"
+API_SECRET = "XXXXXXXXXXXXXXXXXX"
+BASE_URL = "https://paper-api.alpaca.markets"
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': API_SECRET}
 import websocket, json
 
-def on_open(ws):
-    print("opened")
-    auth_data = {
+class live_stream:
+  ws = None
+  
+  def __init__(self):
+    self.alpaca = tradeapi.REST(API_KEY, API_SECRET, BASE_URL, api_version='v2')
+    
+  def run(self):
+    socket = "wss://data.alpaca.markets/stream"
+
+    #Connect to get streaming market data
+    def on_open(ws):
+        #print("opened")
+        auth_data = {
         "action": "authenticate",
-        "data": {"key_id": "PK1X4W2RZG5TB15V0QGM", "secret_key": "dYUMhwHitUflmI6cWNRu2sQHHwwu1LOh9y5DVBa9"}
-    }
+        "data": {"key_id": "XXXXXXXXXXX", "secret_key": "XXXXXXXXXXXXXXX"}
+        }
 
-    ws.send(json.dumps(auth_data))
+        ws.send(json.dumps(auth_data))
 
-    listen_message = {"action": "listen", "data": {"streams": ["T.BA"]}}
+        #AM = Subscribe to minute .* Stock symbol e.g AM.BA
+        listen_message = {"action": "listen", "data": {"streams": ["AM.BA", "AM.MSFT"]}}
+        
+        
+        ws.send(json.dumps(listen_message))
 
-    ws.send(json.dumps(listen_message))
+    #On Message, ever minute bar data we get for the stock we subscribe to 
+    def on_message(ws, message):
+        #Convert our message to dictionary
+        convertedMessage = json.loads(message)
+        try: 
+            #Store vars for candlestick
+            volume = convertedMessage["data"]["v"]
+            open_price = convertedMessage["data"]["o"]
+            close_price = convertedMessage["data"]["c"]
+            high_price = convertedMessage["data"]["h"]
+            low_price = convertedMessage["data"]["l"]
+            average_price = convertedMessage["data"]["a"]
+            ticker = convertedMessage["data"]["T"]   
+            print("Current Volume : ", volume)
+            print("Current Open Price : ", open_price)
+            print("Current Close Price : ", close_price)
+            print("Current High Price : ", high_price)
+            print("Current Low : ", low_price)
+            print("Current Average : ", average_price)
+            print("Symbol : ", ticker)
+           
+        except:
+            pass
+    def on_close(ws):
+        print("Market is closed")
 
+    ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+    ws.run_forever()
+        
 
-def on_message(ws, message):
-    print("received a message")
-    print(message)
-    volume = message.v
-    open_price = message.o
-    close_price = message.c
-    high_price = message.h
-    low_price = message.l
-    average_price = message.a
-    ticker = message.T
-    print(open_price)
-#def on_close(ws):
-    #print("closed connection")
-
-socket = "wss://data.alpaca.markets/stream"
-
-ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message) #on_close=on_close)
-ws.run_forever()
+ls = live_stream()
+ls.run()
