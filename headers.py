cd8e91f7e488d4380b61cbac5dfda0ceabceb13b
Trev18 <66802083+Trev18@users.noreply.github.com>
2021-01-14 08:13:30 -0600

    Create headers.py

---
+++ headers.py
@@ -0,0 +1,11 @@
+### authentication and connection details
+API_KEY = 'API_KEY'
+SECRET_KEY =  'SECRET_KEY'
+
+BASE_URL = "https://paper-api.alpaca.markets"
+ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+HEADERS = {'APCA-API-KEY-ID' : API_KEY, 'APCA-API-SECRET-KEY' : SECRET_KEY}
+
+###Call the API
+api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
